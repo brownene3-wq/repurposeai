18efc042a48b468fa3b5179087c1fb333825e25b
Albert Brown <albertdbrown85@gmail.com>
2026-05-18 23:20:28 +0000

    homepage: Instagram/Facebook/Threads now LIVE (Meta App Review approved)

--- routes/pages.js
+++ routes/pages.js
@@ -432,9 +432,9 @@ router.get('/', async (req, res) => {
           <div class="platform-status-card">
             <h4><span class="live-dot"></span>Platform Status</h4>
             <div class="platform-row"><span class="name">LinkedIn</span><span class="badge badge-on">LIVE</span></div>
-            <div class="platform-row"><span class="name">Instagram</span><span class="badge badge-rev">REVIEW</span></div>
-            <div class="platform-row"><span class="name">Facebook</span><span class="badge badge-rev">REVIEW</span></div>
-            <div class="platform-row"><span class="name">Threads</span><span class="badge badge-rev">REVIEW</span></div>
+            <div class="platform-row"><span class="name">Instagram</span><span class="badge badge-on">LIVE</span></div>
+            <div class="platform-row"><span class="name">Facebook</span><span class="badge badge-on">LIVE</span></div>
+            <div class="platform-row"><span class="name">Threads</span><span class="badge badge-on">LIVE</span></div>
             <div class="platform-row"><span class="name">TikTok</span><span class="badge badge-rev">REVIEW</span></div>
             <div class="platform-row"><span class="name">Pinterest</span><span class="badge badge-rev">REVIEW</span></div>
             <div class="platform-row"><span class="name">YouTube</span><span class="badge badge-soon">SOON</span></div>
